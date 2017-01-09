9353b8db6fb49630e979e811498e32c533856bff
inkcomic <inkcomic@gmail.com>
2017-01-09 18:56:56 +0800

    update

--- Code/CryPlugins/VR/CryHypereal/Module/StdAfx.h
+++ Code/CryPlugins/VR/CryHypereal/Module/StdAfx.h
@@ -1,9 +1,7 @@
-// Copyright 2001-2016 Crytek GmbH / Crytek Group. All rights reserved.
-
 #pragma once
 
 #define eCryModule eCryM_EnginePlugin
 
 #include <CryCore/Platform/platform.h>
 
-#include <openvr.h>
+#include <Hypereal_VR.h>
